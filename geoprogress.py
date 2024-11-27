5ed654e0f21d8709eb9964eecf4f40c10aaa7449
qwerty1121gif <Vladimirstalin@tutamail.com>
2024-11-27 16:45:00 +0300

    q

--- geoprogress.py
+++ geoprogress.py
@@ -1,18 +1,13 @@
 input_data = open('input.txt','r') 
-n = input_data.read()
-n=int(n)
-k=0
-while n > 0:
-    if n%3==0:
-        n-=3
-        k+=1
-    if n%3==1:
-        n-=1
-        k+=1
-    if n%3==2:
-        n-=2
-        k+=1
+a = input_data.read()
+a=a.split()
+b=int(a[0])
+q=int(a[1])
+n=int(a[2])
+s=0
+for i in range(0,n):
+    s+=b*(q**i)
 output_data = open('output.txt','w')
-output_data.write(str(k))
+output_data.write(str(s))
 input_data.close()
 output_data.close()
